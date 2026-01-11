74cda6060a7dfa969251f53e56ee1ff16c174157
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-01-11 22:14:56 +0200

    feat(ui): update SplashScreen widget
    - check if user is already logged in.

--- lib/features/splash_screen/view.dart
+++ lib/features/splash_screen/view.dart
@@ -1,8 +1,42 @@
+import 'package:final_flutter_project/features/login/view.dart';
+import 'package:final_flutter_project/features/navigation_bar/view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    final user = FirebaseAuth.instance.currentUser;
+
+    Future.delayed(Duration(seconds: 3), () {
+      if (user == null) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) {
+              return LoginScreen();
+            },
+          ),
+        );
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) {
+              return NavBar();
+            },
+          ),
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
