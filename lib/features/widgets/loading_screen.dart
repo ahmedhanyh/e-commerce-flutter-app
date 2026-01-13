c9addfb9db3c69d4186fa1dd23612dafc8cbbfd4
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-01-14 00:27:44 +0200

    feat(loading_screen): add animated loading spinner

--- lib/features/widgets/loading_screen.dart
+++ lib/features/widgets/loading_screen.dart
@@ -1,4 +1,6 @@
+import 'package:final_flutter_project/core/resources/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key, required this.message});
@@ -12,7 +14,17 @@ class LoadingScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           spacing: 20,
-          children: [CircularProgressIndicator(), Text(message)],
+          children: [
+            Text(message, style: TextStyle(
+              fontFamily: "Arimo",
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+              color: Colors.black
+            ),),
+            SpinKitChasingDots(
+            size: 30,
+            color: primaryColor,
+          )],
         ),
       ),
     );
